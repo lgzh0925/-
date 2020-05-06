e8b7ac3bea27315492cfbf5b01a0a89e0fb1ea55
lgzh0925 <lgzh0925@gmail.com>
2020-05-06 13:17:51 +1200

    move to index.js

---
+++ js/index.js
@@ -0,0 +1,13 @@
+var typed = new Typed('.typing', {
+    strings: [
+        '你好啊！',
+        '我叫李冠铮，',
+        '我叫李冠铮，一名前端工程师',
+        '我喜欢研究各式各样的数据！',
+        '有机会可以一起工作么？',
+        '期待与您的联系!',
+    ],
+    startDelay: 800,
+    typeSpeed: 60,
+    backSpeed: 60,
+});
