7813f36d0e6c8e4928b49a143857573e698abbdb
lgzh0925 <lgzh0925@gmail.com>
2020-07-16 14:02:36 +0800

    加英文版

---
+++ js/indexE.js
@@ -0,0 +1,14 @@
+var typed = new Typed('.typing', {
+    strings: [
+        'Hi there！',
+        'My name is Guanzheng Li',
+        'I am a Web Developer',
+        'I do like to measure everything！',
+        'We could work together？',
+        'Still here waiting？',
+        'You have to click now!',
+    ],
+    startDelay: 800,
+    typeSpeed: 60,
+    backSpeed: 60,
+});
